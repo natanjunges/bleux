1f46833f4b52c372a21858fe44498a4b0fbf2cec
natanjunges <natanajunges@gmail.com>
2024-01-14 19:13:00 -0300

    Make all flatpaks use wayland, fix message of apt-disabled in bleux-features, use gnome-console as x-terminal-emulator in bleux-features, make gnome-console follow system theme in bleux-settings

--- packages/bleux-features/features/gnome-console/add.sh
+++ packages/bleux-features/features/gnome-console/add.sh
@@ -15,3 +15,4 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 apt-get install -y --mark-auto gnome-console nautilus-extension-gnome-console
+update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kgx 50
--- packages/bleux-features/features/gnome-console/remove.sh
+++ packages/bleux-features/features/gnome-console/remove.sh
@@ -14,4 +14,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+update-alternatives --remove x-terminal-emulator /usr/bin/kgx
 apt-get purge -y gnome-console nautilus-extension-gnome-console
