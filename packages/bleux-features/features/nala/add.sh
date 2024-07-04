999f55e2b4f5f9f47b13215dd10c6b711cd1bde1
natanjunges <natanajunges@gmail.com>
2024-07-04 11:20:47 -0300

    Update copyright notices

--- packages/bleux-features/features/nala/add.sh
+++ packages/bleux-features/features/nala/add.sh
@@ -1,5 +1,5 @@
 # bleUX, a user-centric desktop Linux distribution
-# Copyright (C) 2023  Natan Junges <natanajunges@gmail.com>
+# Copyright (C) 2023, 2024  Natan Junges <natanajunges@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
