0955d0b6dbeab07680f012271f9483a6c389e870
natanjunges <natanajunges@gmail.com>
2024-07-10 09:49:04 -0300

    Remove non-essential GNOME packages, replace cheese and eog with snapshot and loupe flatpaks

---
+++ packages/bleux-features/features/flatpak/loupe.sh
@@ -0,0 +1,31 @@
+# bleUX, a user-centric desktop Linux distribution
+# Copyright (C) 2024  Natan Junges <natanajunges@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+if ! dpkg-query -f '${db:Status-abbrev}' -W flatpak 2> /dev/null | grep -q '^.i'; then
+    exit 1
+fi
+
+case $1 in
+    add)
+        flatpak install -y --noninteractive --no-related org.gnome.Loupe
+    ;;
+    remove)
+        flatpak remove -y --noninteractive --system org.gnome.Loupe
+    ;;
+    *)
+        exit 1
+    ;;
+esac
