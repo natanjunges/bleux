6101e80659462570e9837dc05e0cb357923fa21d
Natan Junges <natanajunges@gmail.com>
2024-07-26 11:04:10 -0300

    Escape subcommand argument in feature scripts, set error flag to not suppress errors in feature scripts

--- packages/bleux-features/features/snap/firefox.sh
+++ packages/bleux-features/features/snap/firefox.sh
@@ -14,11 +14,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+set -e
+
 if ! dpkg-query -f '${db:Status-abbrev}' -W snapd 2> /dev/null | grep -q '^.i'; then
     no_snap=1
 fi
 
-case $1 in
+case "$1" in
     add)
         if [ $no_snap ]; then
             echo 'The \e[1msnap\e[0m feature is not enabled.' >&2
