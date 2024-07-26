a85958919e46dd09bd838a12278d3f309aa18ffa
Natan Junges <natanajunges@gmail.com>
2024-07-26 11:18:42 -0300

    Escape subcommand argument in feature scripts, set error flag to not suppress errors in feature scripts

--- packages/bleux-features/features/transmission.sh
+++ packages/bleux-features/features/transmission.sh
@@ -14,7 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-case $1 in
+set -e
+
+case "$1" in
     add)
         apt-get install -y --mark-auto transmission-gtk
     ;;
