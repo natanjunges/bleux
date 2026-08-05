d3e2c9cdd43156ae94c3378cc806f96b31d5f8da
Natan Junges <natanajunges@gmail.com>
2026-08-05 16:42:03 -0300

    Fix function name

--- packages/bleux-ux/features/apparmor-enforce.sh
+++ packages/bleux-ux/features/apparmor-enforce.sh
@@ -16,7 +16,7 @@
 
 set -e
 
-aa-unconfine() {
+aa_unconfined() {
     profile="$(echo "${1#/}" | tr / .)"
 
     if [ -f "/usr/apparmor.d/$profile" ] && ! grep -qm 1 'flags=(' "/usr/apparmor.d/$profile"; then
@@ -28,80 +28,80 @@ aa-unconfine() {
 case "$1" in
     disable)
         sudo aa-complain /usr/bin/irssi /usr/sbin/sssd Xorg Xorg_wrap
-        aa-unconfine 1password
-        aa-unconfine Discord
-        aa-unconfine 'MongoDB Compass'
-        aa-unconfine QtWebEngineProcess
-        aa-unconfine balena-etcher
-        aa-unconfine brave
-        aa-unconfine buildah
-        aa-unconfine cam
-        aa-unconfine ch-checkns
-        aa-unconfine ch-run
-        aa-unconfine chrome
-        aa-unconfine chromium
-        aa-unconfine crun
-        aa-unconfine desktop-icons-ng
-        aa-unconfine devhelp
-        aa-unconfine element-desktop
-        aa-unconfine epiphany
-        aa-unconfine evolution
-        aa-unconfine firefox
-        aa-unconfine foliate
-        aa-unconfine geary
-        aa-unconfine github-desktop
-        aa-unconfine goldendict
-        aa-unconfine kchmviewer
-        aa-unconfine keybase
-        aa-unconfine lc-compliance
-        aa-unconfine libcamerify
-        aa-unconfine linux-sandbox
-        aa-unconfine loupe
-        aa-unconfine lxc-attach
-        aa-unconfine lxc-create
-        aa-unconfine lxc-destroy
-        aa-unconfine lxc-execute
-        aa-unconfine lxc-stop
-        aa-unconfine lxc-unshare
-        aa-unconfine lxc-usernsexec
-        aa-unconfine mmdebstrap
-        aa-unconfine msedge
-        aa-unconfine notepadqq
-        aa-unconfine obsidian
-        aa-unconfine opam
-        aa-unconfine opera
-        aa-unconfine pageedit
-        aa-unconfine podman
-        aa-unconfine polypane
-        aa-unconfine privacybrowser
-        aa-unconfine qcam
-        aa-unconfine qmapshack
-        aa-unconfine qutebrowser
-        aa-unconfine rootlesskit
-        aa-unconfine rpm
-        aa-unconfine rssguard
-        aa-unconfine runc
-        aa-unconfine scide
-        aa-unconfine signal-desktop
-        aa-unconfine slack
-        aa-unconfine slirp4netns
-        aa-unconfine steam
-        aa-unconfine stress-ng
-        aa-unconfine surfshark
-        aa-unconfine systemd-coredump
-        aa-unconfine thunderbird
-        aa-unconfine trinity
-        aa-unconfine tup
-        aa-unconfine tuxedo-control-center
-        aa-unconfine userbindmount
-        aa-unconfine uwsgi-core
-        aa-unconfine vdens
-        aa-unconfine virtiofsd
-        aa-unconfine vivaldi-bin
-        aa-unconfine vpnns
-        aa-unconfine vscode
-        aa-unconfine wike
-        aa-unconfine wpcom
+        aa_unconfined 1password
+        aa_unconfined Discord
+        aa_unconfined 'MongoDB Compass'
+        aa_unconfined QtWebEngineProcess
+        aa_unconfined balena-etcher
+        aa_unconfined brave
+        aa_unconfined buildah
+        aa_unconfined cam
+        aa_unconfined ch-checkns
+        aa_unconfined ch-run
+        aa_unconfined chrome
+        aa_unconfined chromium
+        aa_unconfined crun
+        aa_unconfined desktop-icons-ng
+        aa_unconfined devhelp
+        aa_unconfined element-desktop
+        aa_unconfined epiphany
+        aa_unconfined evolution
+        aa_unconfined firefox
+        aa_unconfined foliate
+        aa_unconfined geary
+        aa_unconfined github-desktop
+        aa_unconfined goldendict
+        aa_unconfined kchmviewer
+        aa_unconfined keybase
+        aa_unconfined lc-compliance
+        aa_unconfined libcamerify
+        aa_unconfined linux-sandbox
+        aa_unconfined loupe
+        aa_unconfined lxc-attach
+        aa_unconfined lxc-create
+        aa_unconfined lxc-destroy
+        aa_unconfined lxc-execute
+        aa_unconfined lxc-stop
+        aa_unconfined lxc-unshare
+        aa_unconfined lxc-usernsexec
+        aa_unconfined mmdebstrap
+        aa_unconfined msedge
+        aa_unconfined notepadqq
+        aa_unconfined obsidian
+        aa_unconfined opam
+        aa_unconfined opera
+        aa_unconfined pageedit
+        aa_unconfined podman
+        aa_unconfined polypane
+        aa_unconfined privacybrowser
+        aa_unconfined qcam
+        aa_unconfined qmapshack
+        aa_unconfined qutebrowser
+        aa_unconfined rootlesskit
+        aa_unconfined rpm
+        aa_unconfined rssguard
+        aa_unconfined runc
+        aa_unconfined scide
+        aa_unconfined signal-desktop
+        aa_unconfined slack
+        aa_unconfined slirp4netns
+        aa_unconfined steam
+        aa_unconfined stress-ng
+        aa_unconfined surfshark
+        aa_unconfined systemd-coredump
+        aa_unconfined thunderbird
+        aa_unconfined trinity
+        aa_unconfined tup
+        aa_unconfined tuxedo-control-center
+        aa_unconfined userbindmount
+        aa_unconfined uwsgi-core
+        aa_unconfined vdens
+        aa_unconfined virtiofsd
+        aa_unconfined vivaldi-bin
+        aa_unconfined vpnns
+        aa_unconfined vscode
+        aa_unconfined wike
+        aa_unconfined wpcom
     ;;
     enable)
         sudo aa-enforce /usr/bin/irssi /usr/sbin/sssd Xorg Xorg_wrap 1password Discord 'MongoDB Compass' QtWebEngineProcess balena-etcher brave buildah cam ch-checkns ch-run chrome chromium crun desktop-icons-ng devhelp element-desktop epiphany evolution firefox foliate geary github-desktop goldendict kchmviewer keybase lc-compliance libcamerify linux-sandbox loupe lxc-attach lxc-create lxc-destroy lxc-execute lxc-stop lxc-unshare lxc-usernsexec mmdebstrap msedge notepadqq obsidian opam opera pageedit podman polypane privacybrowser qcam qmapshack qutebrowser rootlesskit rpm rssguard runc scide signal-desktop slack slirp4netns steam stress-ng surfshark systemd-coredump thunderbird trinity tup tuxedo-control-center userbindmount uwsgi-core vdens virtiofsd vivaldi-bin vpnns vscode wike wpcom
